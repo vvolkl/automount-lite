2336c62bb60deb96948bc715c216b72e50e0e610
Ian Kent <raven@themaw.net>
2016-08-26 10:25:47 +0800

    autofs-5.1.4 - remove unused function dump_state_queue()
    Signed-off-by: Ian Kent <raven@themaw.net>

--- daemon/state.c
+++ daemon/state.c
@@ -52,30 +52,6 @@ void st_mutex_unlock(void)
 		fatal(status);
 }
 
-void dump_state_queue(void)
-{
-	struct list_head *head = &state_queue;
-	struct list_head *p, *q;
-
-	logmsg("dumping queue");
-
-	list_for_each(p, head) {
-		struct state_queue *entry;
-
-		entry = list_entry(p, struct state_queue, list);
-		logmsg("queue list head path %s state %d busy %d",
-		      entry->ap->path, entry->state, entry->busy);
-
-		list_for_each(q, &entry->pending) {
-			struct state_queue *this;
-
-			this = list_entry(q, struct state_queue, pending);
-			logmsg("queue list entry path %s state %d busy %d",
-			      this->ap->path, this->state, this->busy);
-		}
-	}
-}
-
 void nextstate(int statefd, enum states next)
 {
 	char buf[MAX_ERR_BUF];
