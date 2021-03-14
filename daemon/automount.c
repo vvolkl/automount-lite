vvolkl/automount-lite

commit e68f07f47a483e6f3f420da261d186e740013418

autofs-5.1.2 - add master read wait option
Add command line and configuration options to set the amount of time to
wait for the master map to become available at program start.

Signed-off-by: Ian Kent <raven@themaw.net>

daemon/automount.c
@@ -2103,10 +2103,11 @@ int main(int argc, char *argv[])
 	unsigned ghost, logging, daemon_check;
 	unsigned dumpmaps, foreground, have_global_options;
 	unsigned master_read;
+	int master_wait;
 	time_t timeout;
 	time_t age = monotonic_time(NULL);
 	struct rlimit rlim;
-	const char *options = "+hp:t:vmdD:fVrO:l:n:CF";
+	const char *options = "+hp:t:vmdD:fVrO:l:n:CFM";
 	static const struct option long_options[] = {
 		{"help", 0, 0, 'h'},
 		{"pid-file", 1, 0, 'p'},
@@ -2123,6 +2124,7 @@ int main(int argc, char *argv[])
 		{"set-log-priority", 1, 0, 'l'},
 		{"dont-check-daemon", 0, 0, 'C'},
 		{"force", 0, 0, 'F'},
+		{"master-wait", 1, 0, 'M'},
 		{0, 0, 0, 0}
 	};
 
@@ -2143,6 +2145,7 @@ int main(int argc, char *argv[])
 	nfs_mount_uses_string_options = check_nfs_mount_version(&vers, &check);
 
 	kpkt_len = get_kpkt_len();
+	master_wait = defaults_get_master_wait();
 	timeout = defaults_get_timeout();
 	ghost = defaults_get_browse_mode();
 	logging = defaults_get_logging();
@@ -2200,6 +2203,10 @@ int main(int argc, char *argv[])
 
 		case 'm':
 			dumpmaps = 1;
+			break;
+
+		case 'M':
+			master_wait = getnumopt(optarg, opt);
 			break;
 
 		case 'O':
@@ -2525,7 +2532,7 @@ int main(int argc, char *argv[])
 		 * a signal is received, in which case exit returning an
 		 * error.
 		 */
-		if (!do_master_read_master(master_list, -1)) {
+		if (!do_master_read_master(master_list, master_wait)) {
 			logerr("%s: failed to read master map!", program);
 			master_kill(master_list);
 			release_flag_file();

include/defaults.h
@@ -25,6 +25,7 @@
 #define DEFAULT_MASTER_MAP_NAME	"auto.master"
 
 #define DEFAULT_TIMEOUT			"600"
+#define DEFAULT_MASTER_WAIT		"-1"
 #define DEFAULT_NEGATIVE_TIMEOUT	"60"
 #define DEFAULT_MOUNT_WAIT		"-1"
 #define DEFAULT_UMOUNT_WAIT		"12"
@@ -152,6 +153,7 @@ void defaults_conf_release(void);
 const char *defaults_get_master_map(void);
 int defaults_master_set(void);
 unsigned int defaults_get_timeout(void);
+int defaults_get_master_wait(void);
 unsigned int defaults_get_negative_timeout(void);
 unsigned int defaults_get_browse_mode(void);
 unsigned int defaults_get_logging(void);

lib/defaults.c
@@ -48,6 +48,7 @@
 #define NAME_MASTER_MAP			"master_map_name"
 
 #define NAME_TIMEOUT			"timeout"
+#define NAME_MASTER_WAIT		"master_wait"
 #define NAME_NEGATIVE_TIMEOUT		"negative_timeout"
 #define NAME_BROWSE_MODE		"browse_mode"
 #define NAME_LOGGING			"logging"
@@ -285,6 +286,11 @@ static int conf_load_autofs_defaults(void)
 
 	ret = conf_update(sec, NAME_TIMEOUT,
 			  DEFAULT_TIMEOUT, CONF_ENV);
+	if (ret == CFG_FAIL)
+		goto error;
+
+	ret = conf_update(sec, NAME_MASTER_WAIT,
+			  DEFAULT_MASTER_WAIT, CONF_ENV);
 	if (ret == CFG_FAIL)
 		goto error;
 
@@ -1567,6 +1573,17 @@ unsigned int defaults_get_timeout(void)
 		timeout = atol(DEFAULT_TIMEOUT);
 
 	return (unsigned int) timeout;
+}
+
+int defaults_get_master_wait(void)
+{
+	long wait;
+
+	wait = conf_get_number(autofs_gbl_sec, NAME_MASTER_WAIT);
+	if (wait < 0)
+		wait = atol(DEFAULT_MASTER_WAIT);
+
+	return (int) wait;
 }
 
 unsigned int defaults_get_negative_timeout(void)

man/autofs.conf.5.in
@@ -30,6 +30,11 @@ default is 10 minutes, but the default installed configuration
 overrides this and sets the timeout to 5 minutes to be consistent
 with earlier autofs releases.
 .TP
+.B master_wait
+sets the default maximum time to wait for the master map to become
+available if it cannot be read at program start (program default -1,
+wait forever).
+.TP
 .B negative_timeout
 .br
 Set the default timeout for caching failed key lookups (program default

man/automount.8
@@ -37,6 +37,10 @@ The internal program default is 10 minutes, but the default
 installed configuration overrides this and sets the timeout
 to 5 minutes to be consistent with earlier autofs releases.
 .TP
+.I "\-M <seconds>, \-\-master-wait <seconds>"
+Set the maximum time to wait for the master map to become available
+if it cannot be read at program start.
+.TP
 .I "\-n <seconds>, \-\-negative\-timeout <seconds>"
 Set the default timeout for caching failed key lookups. The default is 60 seconds.
 .TP

redhat/autofs.conf.default.in
@@ -14,6 +14,13 @@
 #
 timeout = 300
 #
+# master_wait - set the default maximum time to wait for the
+# 		master map to become available if it cannot
+# 		be read at program start (default -1, wait
+# 		forever).
+#
+#master_wait = -1
+#
 # negative_timeout - set the default negative timeout for
 # 		     failed mount attempts (default 60).
 #

samples/autofs.conf.default.in
@@ -14,6 +14,13 @@
 #
 timeout = 300
 #
+# master_wait - set the default maximum time to wait for the
+# 		master map to become available if it cannot
+# 		be read at program start (default -1, wait
+# 		forever).
+#
+# master_wait = -1
+#
 # negative_timeout - set the default negative timeout for
 # 		     failed mount attempts (default 60).
 #

commit c3e6314b83054268aa4c66290bcc13235e4e6354

autofs-5.1.7 - Fix option for master read wait
The master-wait program option expects a value, and if provided
automount crashes with the following trace:

#0  __GI_____strtoul_l_internal (nptr=0x0, endptr=0x7fffffffe120, base=0, group=<optimized out>,
    loc=0x7ffff77a63a0 <_nl_global_locale>) at ../stdlib/strtol_l.c:292
#1  0x0000555555562c52 in getnumopt ()
#2  0x0000555555564ec0 in main ()

This is because the options string is not correct and does not expect
an argument for master-wait (M), which sets optarg to NULL.

Fixes: e68f07f ("autofs-5.1.2 - add master read wait option")
Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
Signed-off-by: Ian Kent <raven@themaw.net>

daemon/automount.c
@@ -2274,7 +2274,7 @@ int main(int argc, char *argv[])
 	time_t timeout;
 	time_t age = monotonic_time(NULL);
 	struct rlimit rlim;
-	const char *options = "+hp:t:vmdD:SfVrO:l:n:CFUM";
+	const char *options = "+hp:t:vmdD:SfVrO:l:n:CFUM:";
 	static const struct option long_options[] = {
 		{"help", 0, 0, 'h'},
 		{"pid-file", 1, 0, 'p'},