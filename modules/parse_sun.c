vvolkl/automount-lite

commit 9047e91ffa6950fac9fc3e2706a0faf3f65cd952

autofs-5.1.7 - fix concat_options() error handling
There's a possibility of a memory leak in the mount options processing
when calling concat_options() in parse_mount() of the Sun format map
entry parsing.

There's also a case in do_init() of the Sun map format parsing where
a previously freed value is used in a logging statement without being
set to MULL.

So ensure concat_options() always frees it's arguments so that the
handling can be consistent in all places.

Signed-off-by: Ian Kent <raven@themaw.net>

modules/parse_sun.c
@@ -380,7 +380,8 @@ static int do_init(int argc, const char *const *argv, struct parse_context *ctxt
 			if (!tmp) {
 				char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 				logerr(MODPREFIX "concat_options: %s", estr);
-				free(gbl_options);
+				/* freed in concat_options */
+				ctxt->optstr = NULL;
 			} else
 				ctxt->optstr = tmp;
 		} else {
@@ -492,12 +493,16 @@ static char *concat_options(char *left, char *right)
 	char *ret;
 
 	if (left == NULL || *left == '\0') {
+		if (!right || *right == '\0')
+			return NULL;
 		ret = strdup(right);
 		free(right);
 		return ret;
 	}
 
 	if (right == NULL || *right == '\0') {
+		if (left == NULL || *left == '\0')
+			return NULL;
 		ret = strdup(left);
 		free(left);
 		return ret;
@@ -508,6 +513,8 @@ static char *concat_options(char *left, char *right)
 	if (ret == NULL) {
 		char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 		logerr(MODPREFIX "malloc: %s", estr);
+		free(left);
+		free(right);
 		return NULL;
 	}
 
@@ -989,14 +996,13 @@ static int parse_mapent(const char *ent, char *g_options, char **options, char *
 			if (newopt && strstr(newopt, myoptions)) {
 				free(myoptions);
 				myoptions = newopt;
-			} else {
+			} else if (newopt) {
 				tmp = concat_options(myoptions, newopt);
 				if (!tmp) {
 					char *estr;
 					estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(logopt, MODPREFIX
 					      "concat_options: %s", estr);
-					free(myoptions);
 					return 0;
 				}
 				myoptions = tmp;
@@ -1358,16 +1364,12 @@ dont_expand:
 			if (mnt_options && noptions && strstr(noptions, mnt_options)) {
 				free(mnt_options);
 				mnt_options = noptions;
-			} else {
+			} else if (noptions) {
 				tmp = concat_options(mnt_options, noptions);
 				if (!tmp) {
 					char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(ap->logopt,
 					      MODPREFIX "concat_options: %s", estr);
-					if (noptions)
-						free(noptions);
-					if (mnt_options)
-						free(mnt_options);
 					free(options);
 					free(pmapent);
 					return 1;
@@ -1387,15 +1389,11 @@ dont_expand:
 			if (options && mnt_options && strstr(mnt_options, options)) {
 				free(options);
 				options = mnt_options;
-			} else {
+			} else if (mnt_options) {
 				tmp = concat_options(options, mnt_options);
 				if (!tmp) {
 					char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(ap->logopt, MODPREFIX "concat_options: %s", estr);
-					if (options)
-						free(options);
-					if (mnt_options)
-						free(mnt_options);
 					free(pmapent);
 					return 1;
 				}

commit b6d4169a264d964b811fd8ff74e68145fc9a13de

autofs-5.1.8 - fix minus only option handling in concat_options()
While a '-' alone isn't strictly valid it hadn't previously cuased a
parse error. So commit 9047e91ffa69 (autofs-5.1.7 - fix concat_options()
error handling) introduced a regression by no longer allowing this.

Fix this regression by only failing if errno is set to a non-zero value
on return from concat_options() as well as returning NULL.

Fixes: 9047e91ffa69 (autofs-5.1.7 - fix concat_options() error handling)
Signed-off-by: Ian Kent <raven@themaw.net>

modules/parse_sun.c
@@ -376,10 +376,16 @@ static int do_init(int argc, const char *const *argv, struct parse_context *ctxt
 	if (gbl_options) {
 		append_options = defaults_get_append_options();
 		if (append_options) {
-			char *tmp = concat_options(gbl_options, ctxt->optstr);
+			char *tmp;
+
+			errno = 0;
+			tmp = concat_options(gbl_options, ctxt->optstr);
 			if (!tmp) {
-				char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
-				logerr(MODPREFIX "concat_options: %s", estr);
+				/* Ignore non-error NULL return */
+				if (errno) {
+					char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
+					logerr(MODPREFIX "concat_options: %s", estr);
+				}
 				/* freed in concat_options */
 				ctxt->optstr = NULL;
 			} else
@@ -1007,9 +1013,12 @@ static int parse_mapent(const char *ent, char *g_options, char **options, char *
 				free(myoptions);
 				myoptions = newopt;
 			} else if (newopt) {
+				errno = 0;
 				tmp = concat_options(myoptions, newopt);
-				if (!tmp) {
+				/* Ignore non-error NULL return */
+				if (!tmp && errno) {
 					char *estr;
+
 					estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(logopt, MODPREFIX
 					      "concat_options: %s", estr);
@@ -1381,8 +1390,10 @@ dont_expand:
 				free(mnt_options);
 				mnt_options = noptions;
 			} else if (noptions) {
+				errno = 0;
 				tmp = concat_options(mnt_options, noptions);
-				if (!tmp) {
+				/* Ignore non-error NULL return */
+				if (!tmp && errno) {
 					char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(ap->logopt,
 					      MODPREFIX "concat_options: %s", estr);
@@ -1406,8 +1417,10 @@ dont_expand:
 				free(options);
 				options = mnt_options;
 			} else if (mnt_options) {
+				errno = 0;
 				tmp = concat_options(options, mnt_options);
-				if (!tmp) {
+				/* Ignore non-error NULL return */
+				if (!tmp && errno) {
 					char *estr = strerror_r(errno, buf, MAX_ERR_BUF);
 					error(ap->logopt, MODPREFIX "concat_options: %s", estr);
 					free(pmapent);